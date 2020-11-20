79ded9be4603e77ea803fed8fb10636d2119ae19
gianmarcog <marco.giretti@web.de>
2020-11-20 18:25:33 +0100

    Initial Commit

---
+++ Text Adventure App/Text_Adventure_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_Adventure_AppApp.swift
+//  Text Adventure App
+//
+//  Created by Gianmarco Giretti on 20.11.20.
+//
+
+import SwiftUI
+
+@main
+struct Text_Adventure_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
